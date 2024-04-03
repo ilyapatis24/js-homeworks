d55c694c7db31df7910cadcfcac23146e2245bdf
ilyapatis24 <ilyapatis24@mail.ru>
2024-04-04 00:11:06 +0300

    third mission is completely ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    return users.filter(user => user.gender === gender).
+      reduce((average, userAge, index, array) => average + userAge.age / array.length, 0);
+  }
